195e7846c06102c634e43192f78a45c64f4b36ce
matija <matija.matovic99@gmail.com>
2021-11-10 19:03:08 +0100

    Added initial files

---
+++ kits_nvt/src/main/java/com/projekat/kits_nvt/KitsNvtApplication.java
@@ -0,0 +1,13 @@
+package com.projekat.kits_nvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitsNvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitsNvtApplication.class, args);
+	}
+
+}
---
+++ kits_nvt/src/test/java/com/projekat/kits_nvt/KitsNvtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekat.kits_nvt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitsNvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
