cb37d251ba29c7c0a2992816d2b7f013dc8d2696
jovandjordjic <44474779+jovandjordjic@users.noreply.github.com>
2021-11-11 19:49:35 +0100

    implementirane CRUD operacije nad pićima

--- src/main/java/com/rokzasok/serveit/controller/DrinkController.java
+++ src/main/java/com/rokzasok/serveit/controller/DrinkController.java
@@ -1,13 +1,79 @@
 package com.rokzasok.serveit.controller;
 
+import com.rokzasok.serveit.converters.DrinkDTOtoDrink;
+import com.rokzasok.serveit.converters.DrinkToDrinkDTO;
+import com.rokzasok.serveit.dto.DrinkDTO;
+import com.rokzasok.serveit.model.Drink;
 import com.rokzasok.serveit.service.IDrinkService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/drinks")
 public class DrinkController {
-    @Autowired
-    IDrinkService drinkService;
+    final IDrinkService drinkService;
+    final DrinkToDrinkDTO drinkToDrinkDTO;
+    final DrinkDTOtoDrink drinkDTOtoDrink;
+
+
+    public DrinkController(IDrinkService drinkService, DrinkToDrinkDTO drinkToDrinkDTO, DrinkDTOtoDrink drinkDTOtoDrink) {
+        this.drinkService = drinkService;
+        this.drinkToDrinkDTO = drinkToDrinkDTO;
+        this.drinkDTOtoDrink = drinkDTOtoDrink;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DrinkDTO>> getDrinks() {
+        List<Drink> drinks = drinkService.findAll();
+
+        List<DrinkDTO> dtoDrinks = drinkToDrinkDTO.convert(drinks);
+
+        return new ResponseEntity<>(dtoDrinks, HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DrinkDTO> getDrink(@PathVariable Integer id) {
+        Drink drink = drinkService.findOne(id);
+
+        if (drink == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(drinkToDrinkDTO.convert(drink), HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<DrinkDTO> saveDrink(@RequestBody DrinkDTO drinkDTO) {
+        Drink drink = drinkDTOtoDrink.convert(drinkDTO);
+        drink = drinkService.save(drink);
+
+        return new ResponseEntity<>(drinkToDrinkDTO.convert(drink), HttpStatus.OK);
+    }
+
+    @PutMapping(consumes = "application/json")
+    public ResponseEntity<DrinkDTO> updateDrink(@RequestBody DrinkDTO drinkDTO) {
+        Drink drink = drinkService.findOne(drinkDTO.getId());
+
+        if (drink == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Drink newDrink = drinkDTOtoDrink.convert(drinkDTO);
+
+        drink = drinkService.save(newDrink);
+        return new ResponseEntity<>(drinkToDrinkDTO.convert(drink), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Boolean> deleteDrink(@PathVariable Integer id) {
+        return new ResponseEntity<>(drinkService.deleteOne(id), HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/rokzasok/serveit/service/impl/DrinkService.java
+++ src/main/java/com/rokzasok/serveit/service/impl/DrinkService.java
@@ -1,30 +1,46 @@
 package com.rokzasok.serveit.service.impl;
 
 import com.rokzasok.serveit.model.Drink;
+import com.rokzasok.serveit.repository.DrinkRepository;
 import com.rokzasok.serveit.service.IDrinkService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DrinkService implements IDrinkService {
+
+    private final DrinkRepository drinkRepository;
+
+    public DrinkService(DrinkRepository drinkRepository) {
+        this.drinkRepository = drinkRepository;
+    }
+
     @Override
     public List<Drink> findAll() {
-        return null;
+        return drinkRepository.findAll();
     }
 
     @Override
     public Drink findOne(Integer id) {
-        return null;
+        return drinkRepository.findById(id).orElse(null);
     }
 
     @Override
     public Drink save(Drink entity) {
-        return null;
+        return drinkRepository.save(entity);
     }
 
     @Override
     public Boolean deleteOne(Integer id) {
-        return null;
+        Drink drink = findOne(id);
+
+        if (drink == null) {
+            return false;
+        }
+
+        drinkRepository.delete(drink);
+        return true;
     }
 }
