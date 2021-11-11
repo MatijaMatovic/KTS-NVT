e6d0062341e5f7c3f167f725256c91f7661386ee
jovandjordjic <44474779+jovandjordjic@users.noreply.github.com>
2021-11-11 20:00:33 +0100

    dodati komentari

--- src/main/java/com/rokzasok/serveit/controller/DrinkController.java
+++ src/main/java/com/rokzasok/serveit/controller/DrinkController.java
@@ -13,20 +13,44 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * The type Drink controller.
+ */
 @RestController
 @RequestMapping("api/drinks")
 public class DrinkController {
+    /**
+     * The Drink service.
+     */
     final IDrinkService drinkService;
+    /**
+     * The Drink to drink dto.
+     */
     final DrinkToDrinkDTO drinkToDrinkDTO;
+    /**
+     * The Drink dto to drink.
+     */
     final DrinkDTOtoDrink drinkDTOtoDrink;
 
 
+    /**
+     * Instantiates a new Drink controller.
+     *
+     * @param drinkService    the drink service
+     * @param drinkToDrinkDTO the drink to drink dto
+     * @param drinkDTOtoDrink the drink dt oto drink
+     */
     public DrinkController(IDrinkService drinkService, DrinkToDrinkDTO drinkToDrinkDTO, DrinkDTOtoDrink drinkDTOtoDrink) {
         this.drinkService = drinkService;
         this.drinkToDrinkDTO = drinkToDrinkDTO;
         this.drinkDTOtoDrink = drinkDTOtoDrink;
     }
 
+    /**
+     * Gets drinks.
+     *
+     * @return the drinks
+     */
     @GetMapping
     public ResponseEntity<List<DrinkDTO>> getDrinks() {
         List<Drink> drinks = drinkService.findAll();
@@ -37,6 +61,12 @@ public class DrinkController {
 
     }
 
+    /**
+     * Gets drink.
+     *
+     * @param id the id
+     * @return the drink
+     */
     @GetMapping(value = "/{id}")
     public ResponseEntity<DrinkDTO> getDrink(@PathVariable Integer id) {
         Drink drink = drinkService.findOne(id);
@@ -48,6 +78,12 @@ public class DrinkController {
         return new ResponseEntity<>(drinkToDrinkDTO.convert(drink), HttpStatus.OK);
     }
 
+    /**
+     * Save drink.
+     *
+     * @param drinkDTO the drink dto
+     * @return the response entity
+     */
     @PostMapping(consumes = "application/json")
     public ResponseEntity<DrinkDTO> saveDrink(@RequestBody DrinkDTO drinkDTO) {
         Drink drink = drinkDTOtoDrink.convert(drinkDTO);
@@ -56,6 +92,12 @@ public class DrinkController {
         return new ResponseEntity<>(drinkToDrinkDTO.convert(drink), HttpStatus.OK);
     }
 
+    /**
+     * Update drink.
+     *
+     * @param drinkDTO the drink dto
+     * @return the response entity
+     */
     @PutMapping(consumes = "application/json")
     public ResponseEntity<DrinkDTO> updateDrink(@RequestBody DrinkDTO drinkDTO) {
         Drink drink = drinkService.findOne(drinkDTO.getId());
@@ -70,6 +112,12 @@ public class DrinkController {
         return new ResponseEntity<>(drinkToDrinkDTO.convert(drink), HttpStatus.OK);
     }
 
+    /**
+     * Delete drink.
+     *
+     * @param id the id
+     * @return the response entity
+     */
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Boolean> deleteDrink(@PathVariable Integer id) {
         return new ResponseEntity<>(drinkService.deleteOne(id), HttpStatus.OK);
