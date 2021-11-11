0f0f495b0924274b2a2a3d3c85a603b79f35fb86
jovandjordjic <44474779+jovandjordjic@users.noreply.github.com>
2021-11-11 20:26:40 +0100

    implementirane CRUD operacije nad jelima

---
+++ src/main/java/com/rokzasok/serveit/converters/DishToDishDTO.java
@@ -0,0 +1,36 @@
+package com.rokzasok.serveit.converters;
+
+import com.rokzasok.serveit.dto.DishDTO;
+import com.rokzasok.serveit.model.Dish;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DishToDishDTO implements Converter<Dish, DishDTO> {
+    @Override
+    public DishDTO convert(Dish source) {
+        return new DishDTO(
+                source.getId(),
+                source.getCategory(),
+                source.getCode(),
+                source.getAllergens(),
+                source.getIngredients(),
+                source.getRecipe(),
+                source.getPreparationPrice(),
+                source.getDescription(),
+                source.getImagePath(),
+                source.getPreparationTime()
+        );
+    }
+
+    public List<DishDTO> convert(List<Dish> dishes) {
+        List<DishDTO> dtoList = new ArrayList<>();
+        for (Dish d : dishes) {
+            dtoList.add(convert(d));
+        }
+        return dtoList;
+    }
+}
