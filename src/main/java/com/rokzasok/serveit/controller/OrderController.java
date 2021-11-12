9533f50bdde192dcc8d78a258bfbb46bcfdf1e51
matija <matija.matovic99@gmail.com>
2021-11-12 21:21:48 +0100

    Added converters and implemented some service methods necessary for database manipulation of specified classes.

--- src/main/java/com/rokzasok/serveit/controller/OrderController.java
+++ src/main/java/com/rokzasok/serveit/controller/OrderController.java
@@ -15,4 +15,5 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+
 }
--- src/main/java/com/rokzasok/serveit/service/impl/OrderService.java
+++ src/main/java/com/rokzasok/serveit/service/impl/OrderService.java
@@ -1,30 +1,45 @@
 package com.rokzasok.serveit.service.impl;
 
+import com.rokzasok.serveit.repository.OrderRepository;
 import com.rokzasok.serveit.service.IOrderService;
 import com.rokzasok.serveit.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
 public class OrderService implements IOrderService {
+    final
+    OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @Override
     public List<Order> findAll() {
-        return null;
+        return orderRepository.findAll();
     }
 
     @Override
     public Order findOne(Integer id) {
-        return null;
+        return orderRepository.findById(id).orElse(null);
     }
 
     @Override
     public Order save(Order entity) {
-        return null;
+        return orderRepository.save(entity);
     }
 
     @Override
     public Boolean deleteOne(Integer id) {
-        return null;
+        //TODO: Check if unfinished orders can be deleted
+        Order toDelete = findOne(id);
+        if (toDelete == null)
+            throw new EntityNotFoundException("Order with given ID not found");
+        orderRepository.delete(toDelete);
+        return true;
     }
 }
