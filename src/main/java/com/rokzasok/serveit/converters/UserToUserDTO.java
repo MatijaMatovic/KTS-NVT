bd17f7cf842f1278d1edafce5fca94deea3d6a88
matija <matija.matovic99@gmail.com>
2021-12-06 17:13:46 +0100

    UserDTO is now sent between frontend and backend instead of the whole User class.

--- src/main/java/com/rokzasok/serveit/converters/UserToUserDTO.java
+++ src/main/java/com/rokzasok/serveit/converters/UserToUserDTO.java
@@ -1,8 +1,13 @@
 package com.rokzasok.serveit.converters;
 
 import com.rokzasok.serveit.dto.UserDTO;
+import com.rokzasok.serveit.model.Role;
 import com.rokzasok.serveit.model.User;
+import com.sun.istack.NotNull;
 import org.springframework.core.convert.converter.Converter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserToUserDTO implements Converter<UserDTO, User> {
     @Override
@@ -15,6 +20,33 @@ public class UserToUserDTO implements Converter<UserDTO, User> {
         user.setType(source.getType());
         user.setImagePath(source.getImagePath());
         user.setIsDeleted(false);
+
+        List<Role> roles = new ArrayList<>();
+        switch (source.getType()) {
+            case CHEF:
+                roles.add(new Role("ROLE_CHEF"));
+                break;
+            case COOK:
+                roles.add(new Role("ROLE_COOK"));
+                break;
+            case WAITER:
+                roles.add(new Role("ROLE_WAITER"));
+                break;
+            case BARTENDER:
+                roles.add(new Role("ROLE_BARTENDER"));
+                break;
+            case MANAGER:
+                roles.add(new Role("ROLE_MANAGER"));
+                break;
+            case DIRECTOR:
+                roles.add(new Role("ROLE_DIRECTOR"));
+                break;
+            case ADMINISTRATOR:
+                roles.add(new Role("ROLE_ADMIN"));
+                break;
+        }
+        user.setRoles(roles);
+
         return user;
     }
 }
