45d1b397f094064475660f9cff13145478067e59
matija <matija.matovic99@gmail.com>
2021-12-07 20:28:22 +0100

    Added unit tests for OrderService class.

--- src/main/java/com/rokzasok/serveit/model/Order.java
+++ src/main/java/com/rokzasok/serveit/model/Order.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @SuppressWarnings("LombokEqualsAndHashCodeInspection")
@@ -46,14 +47,14 @@ public class Order {
             joinColumns = @JoinColumn(name = "order_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id"))
     @ToString.Exclude
-    private Set<DishOrderItem> dishes;
+    private Set<DishOrderItem> dishes = new HashSet<>();
 
     @ManyToMany (fetch = FetchType.EAGER)
     @JoinTable(name = "order_drinks",
             joinColumns = @JoinColumn(name = "order_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id"))
     @ToString.Exclude
-    private Set<DrinkOrderItem> drinks;
+    private Set<DrinkOrderItem> drinks = new HashSet<>();
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "waiter_id", referencedColumnName = "id")
