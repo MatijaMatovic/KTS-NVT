29c430bac07ca5871ac90771bbaaf0caf13f6c00
Jovana <klimentaj99@gmail.com>
2021-12-07 15:39:02 +0100

    Izmjene u repozitorijumu dish order item i drink order item

--- src/main/java/com/rokzasok/serveit/service/IDishOrderItemService.java
+++ src/main/java/com/rokzasok/serveit/service/IDishOrderItemService.java
@@ -7,8 +7,8 @@ import com.rokzasok.serveit.model.ItemStatus;
 import java.util.List;
 
 public interface IDishOrderItemService extends IGenericService<DishOrderItem> {
-    public Boolean changeStatusDishOrderItem(Integer id, ItemStatus itemStatus);
-    public Boolean acceptDishOrderItem(Integer id, ItemStatus itemStatus, Integer cookId);
+    public DishOrderItem changeStatusDishOrderItem(Integer id, ItemStatus itemStatus) throws Exception;
+    public DishOrderItem acceptDishOrderItem(Integer id, ItemStatus itemStatus, Integer cookId, IUserService userService) throws Exception;
     List<DishOrderItem> findAllByOrderID(Integer orderID);
     List<DishOrderItem> findAllByCookID(Integer waiterID);
 }
