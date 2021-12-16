d74c5bbd45e26adfdf32fa49db643f07cb466689
Jovana <klimentaj99@gmail.com>
2021-12-16 16:45:31 +0100

    U DishOrderItemController-u i odgovarajucim servisima izmjenjene metode tako da je logika prebacena u servise i da su kreirani odgovarajuci exception-i.

--- src/main/java/com/rokzasok/serveit/service/IDishOrderItemService.java
+++ src/main/java/com/rokzasok/serveit/service/IDishOrderItemService.java
@@ -1,13 +1,13 @@
 package com.rokzasok.serveit.service;
 
+import com.rokzasok.serveit.exceptions.DishOrderItemNotFoundException;
+import com.rokzasok.serveit.exceptions.UserNotFoundException;
 import com.rokzasok.serveit.model.DishOrderItem;
-import com.rokzasok.serveit.model.FoodMenu;
-import com.rokzasok.serveit.model.ItemStatus;
-import com.rokzasok.serveit.model.User;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 public interface IDishOrderItemService extends IGenericService<DishOrderItem> {
     List<DishOrderItem> findAllByCookID(Integer cookId);
+    DishOrderItem acceptDishOrderItem(Integer id, Integer cookId, IUserService userService) throws DishOrderItemNotFoundException, UserNotFoundException;
+    DishOrderItem completeDishOrderItem(Integer id, Integer cookId, IUserService userService) throws DishOrderItemNotFoundException, UserNotFoundException;
 }
