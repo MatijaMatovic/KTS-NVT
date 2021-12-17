6aeb4d83d7dc0985d536931804ab81f948fbcfb3
Jovana <klimentaj99@gmail.com>
2021-12-17 13:57:40 +0100

    Testirana metoda completeDrinkOrderItem iz DrinkOrderItemService-a

--- src/test/java/com/rokzasok/serveit/service/klimenta/unit/DrinkOrderItemUnitTest.java
+++ src/test/java/com/rokzasok/serveit/service/klimenta/unit/DrinkOrderItemUnitTest.java
@@ -1,6 +1,5 @@
 package com.rokzasok.serveit.service.klimenta.unit;
 
-import com.rokzasok.serveit.exceptions.DishOrderItemNotFoundException;
 import com.rokzasok.serveit.exceptions.DrinkOrderItemNotFoundException;
 import com.rokzasok.serveit.exceptions.ItemStatusSetException;
 import com.rokzasok.serveit.exceptions.UserNotFoundException;
@@ -17,7 +16,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Optional;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
@@ -88,6 +86,59 @@ public class DrinkOrderItemUnitTest {
         Mockito.when(drinkOrderItemRepository.save(drinkOrderItem)).thenReturn(drinkOrderItem);
 
         DrinkOrderItem savedDrinkOrderItem = drinkOrderItemService.acceptDrinkOrderItem(DRINK_ORDER_ITEM_ID, BARTENDER_ID, userService);
+        assertNotNull(savedDrinkOrderItem);
+    }
+
+    @Test(expected = DrinkOrderItemNotFoundException.class)
+    public void testCompleteDrinkOrderItem_NonExistingDrinkOrderItem()
+            throws DrinkOrderItemNotFoundException, UserNotFoundException, ItemStatusSetException {
+
+        Mockito.when(drinkOrderItemRepository.findById(NON_EXISTING_ID)).thenReturn(Optional.empty());
+
+        drinkOrderItemService.completeDrinkOrderItem(NON_EXISTING_ID, BARTENDER_ID, userService);
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    public void testCompleteDrinkOrderItem_NonExistingBartender()
+            throws DrinkOrderItemNotFoundException, UserNotFoundException, ItemStatusSetException {
+
+        Mockito.when(drinkOrderItemRepository.findById(DRINK_ORDER_ITEM_ID)).thenReturn(Optional.of(new DrinkOrderItem()));
+        Mockito.when(userService.findOne(NON_EXISTING_ID)).thenReturn(null);
+
+        drinkOrderItemService.completeDrinkOrderItem(DRINK_ORDER_ITEM_ID, NON_EXISTING_ID, userService);
+    }
+
+    @Test(expected = ItemStatusSetException.class)
+    public void testCompleteDrinkOrderItem_WrongItemStatus()
+            throws DrinkOrderItemNotFoundException, UserNotFoundException, ItemStatusSetException {
+
+        DrinkOrderItem doi = new DrinkOrderItem();
+        doi.setStatus(ItemStatus.CREATED);
+        User bartender = new User();
+
+        Mockito.when(drinkOrderItemRepository.findById(DRINK_ORDER_ITEM_ID)).thenReturn(Optional.of(doi));
+        Mockito.when(userService.findOne(BARTENDER_ID)).thenReturn(bartender);
+
+        drinkOrderItemService.completeDrinkOrderItem(DRINK_ORDER_ITEM_ID, BARTENDER_ID, userService);
+    }
+
+    @Test
+    public void testCompleteDrinkOrderItem_CorrectDrinkOrderItem_CorrectBartender_CorrectItemStatus()
+            throws DrinkOrderItemNotFoundException, UserNotFoundException, ItemStatusSetException {
+
+        DrinkOrderItem drinkOrderItem = new DrinkOrderItem();
+        drinkOrderItem.setId(DRINK_ORDER_ITEM_ID);
+        drinkOrderItem.setIsDeleted(false);
+        drinkOrderItem.setStatus(ItemStatus.IN_PROGRESS);
+
+        User bartender = new User();
+        bartender.setId(BARTENDER_ID);
+
+        Mockito.when(drinkOrderItemRepository.findById(DRINK_ORDER_ITEM_ID)).thenReturn(Optional.of(drinkOrderItem));
+        Mockito.when(userService.findOne(BARTENDER_ID)).thenReturn(bartender);
+        Mockito.when(drinkOrderItemRepository.save(drinkOrderItem)).thenReturn(drinkOrderItem);
+
+        DrinkOrderItem savedDrinkOrderItem = drinkOrderItemService.completeDrinkOrderItem(DRINK_ORDER_ITEM_ID, BARTENDER_ID, userService);
         assertNotNull(savedDrinkOrderItem);
     }
 
